9d427ec5b70351caeedf2c5f5b84f6c6b1002188
ForrestDevs <luke.gannon@me.com>
2024-11-21 01:03:08 -0500

    feat: optimze asset sizes

--- src/game/scenes/Boot.ts
+++ src/game/scenes/Boot.ts
@@ -9,12 +9,6 @@ export default class Boot extends Scene {
     //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
     //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
     this.load.pack("pack", "assets/boot-asset-pack.json");
-    this.load.pack("bg", "assets/loadingScreen/loadingPack.json");
-
-    this.load.animation(
-      "loading",
-      "assets/loadingScreen/loadingScreenAnimation.json"
-    );
   }
 
   create() {
--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -8,63 +8,40 @@ import { EventBus } from "../EventBus";
 /* END-USER-IMPORTS */
 
 export default class MainMenu extends Phaser.Scene {
-  private background!: Phaser.GameObjects.Sprite;
+  private background!: Phaser.GameObjects.Video;
+  private music!:
+    | Phaser.Sound.WebAudioSound
+    | Phaser.Sound.NoAudioSound
+    | Phaser.Sound.HTML5AudioSound;
 
   constructor() {
     super("MainMenu");
   }
 
-  editorCreate(): void {
-    // background
-    this.add.image(512, 384, "background");
-
-    // logo
-    const logo = this.add.image(512, 384, "logo");
-
-    // text
-    const text = this.add.text(512, 460, "", {});
-    text.setOrigin(0.5, 0.5);
-    text.text = "Main Menu";
-    text.setStyle({
-      align: "center",
-      color: "#ffffff",
-      fontFamily: "Arial Black",
-      fontSize: "38px",
-      stroke: "#000000",
-      strokeThickness: 8,
-    });
-
-    this.logo = logo;
-
-    this.events.emit("scene-awake");
+  preload() {
+    this.load.video("loading", "assets/loadingScreen/loadingScreen.mp4");
   }
 
-  private logo!: Phaser.GameObjects.Image;
-
-  /* START-USER-CODE */
-  logoTween: Phaser.Tweens.Tween | null;
-
-  // Write your code here
   create() {
-    // this.editorCreate();
-    EventBus.emit("current-scene-ready", this);
-    // Create and play the background animation
-    this.background = this.add
-      .sprite(1280 / 2, 720 / 2, "buddyload_00000")
+    this.add
+      .image(1280 / 2, 720 / 2, "BG95")
       .setDisplaySize(1280, 720)
-      .setDepth(-2)
-      .play("buddyload");
-
-    const bgMusic = this.sound.add("bgMusic");
-    // Start background music
-    bgMusic.play({
+      .setDepth(-3);
+    EventBus.emit("current-scene-ready", this);
+    this.music = this.sound.add("bgMusic");
+    this.music.play({
       loop: true,
       volume: 1,
     });
+    this.background = this.add
+      .video(1280 / 2, 720 / 2, "loading")
+      .setScale(0.68)
+      .setDepth(-2)
+      .play(true);
 
     // Add play button
     const playButton = this.add
-      .text(512, 384, "Play Game", {
+      .text(400, 384, "Play Game", {
         fontSize: "32px",
         color: "#ffffff",
         backgroundColor: "#000000",
@@ -93,37 +70,7 @@ export default class MainMenu extends Phaser.Scene {
     this.background.destroy();
     this.scene.start("Preloader");
   }
-
-  moveLogo(vueCallback: ({ x, y }: { x: number; y: number }) => void) {
-    if (this.logoTween) {
-      if (this.logoTween.isPlaying()) {
-        this.logoTween.pause();
-      } else {
-        this.logoTween.play();
-      }
-    } else {
-      this.logoTween = this.tweens.add({
-        targets: this.logo,
-        x: { value: 750, duration: 3000, ease: "Back.easeInOut" },
-        y: { value: 80, duration: 1500, ease: "Sine.easeOut" },
-        yoyo: true,
-        repeat: -1,
-        onUpdate: () => {
-          if (vueCallback) {
-            vueCallback({
-              x: Math.floor(this.logo.x),
-              y: Math.floor(this.logo.y),
-            });
-          }
-        },
-      });
-    }
-  }
-  /* END-USER-CODE */
 }
 
-/* END OF COMPILED CODE */
-
-// You can write more code here
 export { MainMenu };
 
