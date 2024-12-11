4e894a625722b4c10d949b3861c23a89f56cde98
ForrestDevs <luke.gannon@me.com>
2024-12-11 15:32:08 -0500

    feat: update links

--- src/lib/sets.ts
+++ src/lib/sets.ts
@@ -2,7 +2,7 @@ export const MARKETCAP = "0";
 export const SANTA_SKIN_UNLOCKED = false;
 export const BLOOD_BOX_UNLOCKED = false;
 export const BURNER_UNLOCKED = false;
-export const X_LINK = "https://x.com/kickthebuddy";
-export const TELEGRAM_LINK = "https://t.me/kickthebuddy";
+export const X_LINK = "https://x.com/buddygamesol";
+export const TELEGRAM_LINK = "https://t.me/buddygamesol";
 export const DEX_LINK = "https://dexscreener.com/";
 
